b13651562998c375249e62436af0d08a485b87df
CODEbyPoHsiang <52902326+CODEbyPoHsiang@users.noreply.github.com>
2020-01-14 18:11:20 +0800

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
-result=$(curl -s  https://dinbendon.net/do/login  -c cookies | grep -w "6em"|sed 's/<[^<]*>//g'|sed 's/=//g'|bc )
+result=$(curl -s https://dinbendon.net/do/login -c cookies | tee content.html | grep -w "6em"|sed 's/<[^<]*>//g'|sed 's/=//g'|bc )
 
-posturl=$(curl -s  https://dinbendon.net/do/login | grep -w 'form action="'|grep -oP '(\/do\/;jsessionid=[0-9A-F]+(.*?)")' |sed 's/"//g')
+posturl=$(grep -w 'form action="' content.html | grep -oP '(\/do\/;jsessionid=[0-9A-F]+(.*?)")' |sed 's/"//g')
 
 curl -b cookies -d "signInPanel_signInForm%3Ahf%3A0=&username=guest&password=guest&result=$result&submit=%E7%99%BB%E5%85%A5" -X POST  "https://dinbendon.net$posturl"
 
